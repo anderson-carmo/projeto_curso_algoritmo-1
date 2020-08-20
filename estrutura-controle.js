9eb4231f675728901f1e100b9e50bb415ebe4b25
Anderson Costa Santos do Carmo <acarmo@szd.com.br>
2020-08-20 11:49:16 -0300

    primeiro commit do código

---
+++ estrutura-controle.js
@@ -0,0 +1,31 @@
+/*
+escreval("Digite o nome do aluno: ")
+leia(nome)
+escreval("")
+escreval("Digite a nota 01 do aluno: ")
+leia(nota01)
+escreval("")
+escreval("Digite a nota 02 do aluno: ")
+leia(nota02)
+escreval("")
+media := (nota01 + nota02) / 2
+
+se media >= 5 entao
+   escreval("Aprovado! ", "'",nome,"'")
+senao
+   escreval("Reprovado! ", "'",nome,"'")
+fimse
+*/
+
+var nome, nota01, nota02;
+nome = prompt("Digite o nome do aluno: ")
+nota01 = prompt("Digite a nota 01 do aluno: ")
+nota02 = prompt("Digite a nota 02 do aluno: ")
+
+media = (parseInt(nota01) + parseInt(nota02)) / 2;
+
+if(media >= 50)
+    alert("Aprovado! " + "'" + nome + "'")
+else    
+    alert("Reprovado! " + "'" + nome + "'")
+    
---
+++ input-outpt.js
@@ -0,0 +1,17 @@
+//
+
+
+/*
+fsfsfsffs
+fsdfdfsdf
+fsfsfsfsf
+
+sfsfsfsfs
+*/
+
+
+var nome, idade;
+nome = prompt("Digite o seu nome:")
+idade = prompt("Digite a sua idade:")
+alert("O nome digitado foi: " + nome)
+alert("A idade digitada foi: " + idade)
